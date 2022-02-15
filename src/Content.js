db4d7ad386a5bb6ee0c220a841d59d5ff483290d
Gabriel Ayala <gabeayala100@gmail.com>
2022-02-14 20:00:38 -0800

    Use state to track count each time a button connected to a count function is pressed.

--- src/Content.js
+++ src/Content.js
@@ -1,27 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
+
 
 const Content = () => {
+
+  const [getter, setGetter] = useState('Default Name');
+  const [count, setCount] = useState(0);
   const handleNameChange = () => {
     const names = ['Bob', 'Kevin', 'Dave'];
     const int = Math.floor(Math.random() * 3);
-    return names[int];
+    setGetter(names[int]);
   }
-  const handleClick = () => {
-    console.log("Clicked")
+  const handleCount = () => {
+    setCount(count + 1)
+    console.log(count)
   }
-  const handleClick2 = (name) => {
-    console.log(`${name} was clicked`)
+  const handleClick2 = () => {
+    console.log(count)
   }
   const handleClick3 = (e) => {
     console.log(e.target.innerText)
   }
   return (
     <main>
-      <p onDoubleClick={handleClick}>
-        Hello {handleNameChange()}
+      <p onDoubleClick={handleClick2}>
+        Hello {getter}
       </p>
-      <button onClick={handleClick}>Click</button>
-      <button onClick={() => handleClick2('Dave')}>2</button>
+      <button onClick={handleNameChange}>Change Name</button>
+      <button onClick={handleCount}>2</button>
       <button onClick={(e) => handleClick3(e)}>3</button>
 
     </main>
