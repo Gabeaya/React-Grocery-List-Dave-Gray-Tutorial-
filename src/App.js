cdec65d658dd63b331df9523362708e8e7c1350b
Gabriel Ayala <gabeayala100@gmail.com>
2022-02-14 19:36:54 -0800

    Add a header and content component to put up some beginners text.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Square from "./Square";
 import  Input from "./Input";
+import  {Header}  from "./Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Square />
       <Input />
     </div>
