00abb376f5e5c3ae3b896c4daef11a49bdb9598f
Gabriel Ayala <gabeayala100@gmail.com>
2022-02-14 19:51:11 -0800

    Add an event, doublelick, and annonymous function button.

--- src/Content.js
+++ src/Content.js
@@ -6,11 +6,24 @@ const Content = () => {
     const int = Math.floor(Math.random() * 3);
     return names[int];
   }
+  const handleClick = () => {
+    console.log("Clicked")
+  }
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked`)
+  }
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText)
+  }
   return (
     <main>
-      <p>
-        Hello {handleNameChange()};
+      <p onDoubleClick={handleClick}>
+        Hello {handleNameChange()}
       </p>
+      <button onClick={handleClick}>Click</button>
+      <button onClick={() => handleClick2('Dave')}>2</button>
+      <button onClick={(e) => handleClick3(e)}>3</button>
+
     </main>
   )
 }
