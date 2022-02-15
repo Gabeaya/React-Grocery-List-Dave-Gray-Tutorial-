d904c4ad4ea5227e949ae94b3eedebc2ecfec9c7
Gabriel Ayala <gabeayala100@gmail.com>
2022-02-14 19:24:55 -0800

    Fix import error.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import Square from "./Square";
+import  Input from "./Input";
 
 function App() {
   return (
     <div className="App">
-      
+      <Square />
+      <Input />
     </div>
   );
 }
---
+++ src/Square.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Square() {
+  return (
+    <div>
+      <h2>Square</h2>
+    </div>
+  );
+}
+
+export default Square;
