adef52941254481e91d873fce1c192c8d9435243
Gabriel Ayala <gabeayala100@gmail.com>
2022-02-15 12:31:21 -0800

    Write a function that adds a new item and a function for setting and saving items.

--- src/App.js
+++ src/App.js
@@ -24,25 +24,39 @@ function App() {
   ]);
 
   const [newItem, setNewItem] = useState('');
+  
 
+  const setAndSaveItems = (newItems) => {
+    setItems(newItems);
+    // local storage will store all list items in a collection called shopping list
+    localStorage.setItem('shoppinglist', JSON.stringify(newItems));
+
+  }
+  const addItem = (item) => {
+    // this ternary statement sets the id value for the item.
+    const id = items.length ? items[items.length -  1].id + 1 : 1;
+    const myNewItem = { id, checked: false, item};
+    const listItems = [...items, myNewItem];
+    setAndSaveItems(listItems);
+
+  }
   // set a variable each to a functin that maps through the items, for the item that matches the id as the item that was clicked, we will make a Copy, using the spreadoperator= ...item, of the said item and alter its checked property.
   const handleCheck = (id) => {
     const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked} : item);
-    setItems(listItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
-    // local storage will store all list items in a collection called shopping list
+    setAndSaveItems(listItems);
   }
   const handleDelete = (id) => {
     // this creates a new array with all items except the selected id item
     const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
-
+    setAndSaveItems(listItems);
   }
 
   const handleSubmit = (e) => {
     console.log('submit it')
-    
+    if (!newItem) return;
+    // line 47 resets the input value so it wont stay there after submission
+    setNewItem('');
+    event.preventDefault();
   }
 
   return (
