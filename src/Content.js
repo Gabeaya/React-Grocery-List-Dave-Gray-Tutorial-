fd65c42522d511fccdb2bd51547b7a69bf780264
Gabriel Ayala <gabeayala100@gmail.com>
2022-02-14 20:09:24 -0800

    Add an array to the item state and provide a map for each item.

--- src/Content.js
+++ src/Content.js
@@ -2,33 +2,36 @@ import React, {useState} from 'react'
 
 
 const Content = () => {
-
-  const [getter, setGetter] = useState('Default Name');
-  const [count, setCount] = useState(0);
-  const handleNameChange = () => {
-    const names = ['Bob', 'Kevin', 'Dave'];
-    const int = Math.floor(Math.random() * 3);
-    setGetter(names[int]);
-  }
-  const handleCount = () => {
-    setCount(count + 1)
-    console.log(count)
-  }
-  const handleClick2 = () => {
-    console.log(count)
-  }
-  const handleClick3 = (e) => {
-    console.log(e.target.innerText)
-  }
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "A turtle"
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "toilet paper"
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "toilet"
+    }
+  ])
   return (
     <main>
-      <p onDoubleClick={handleClick2}>
-        Hello {getter}
-      </p>
-      <button onClick={handleNameChange}>Change Name</button>
-      <button onClick={handleCount}>2</button>
-      <button onClick={(e) => handleClick3(e)}>3</button>
-
+      <ul>
+        {items.map((item) => (
+          <li className='item' key={item.id}>
+            <input
+            type="checkbox"
+            checked={item.checked}/>
+            <lable>{item.item}</lable>
+            <button>Delete</button>
+          </li>
+        ))}
+      </ul>
     </main>
   )
 }
