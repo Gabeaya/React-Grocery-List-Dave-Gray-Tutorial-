935413776147d03d64291a506be8c9a78b2bd7a9
Gabriel Ayala <gabeayala100@gmail.com>
2022-02-14 20:36:14 -0800

    Add a decoration to all the checked todos.

--- src/Content.js
+++ src/Content.js
@@ -36,6 +36,7 @@ const Content = () => {
             onChange={() => handleCheck(item.id)}
             checked={item.checked}/>
             <label
+            style={(item.checked) ? { textDecoration: 'line-through'} : null}
               onDoubleClick={() => handleCheck(item.id)}
             >{item.item}</label>
             <FaTrashAlt role="button" tabIndex="0"/>
