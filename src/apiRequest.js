1e757c07b4b056498e1e0cb11acc6e35d520eec2
Gabriel Ayala <gabeayala100@gmail.com>
2022-02-26 11:55:44 -0800

    Add a commment to denote waht the component is for.

--- src/apiRequest.js
+++ src/apiRequest.js
@@ -1,7 +1,8 @@
+//This component is responsible for handling predefined crud actions within app.js
 const apiRequest = async (url = '', optionsObj = null, errMsg = null) => {
   try {
     const response = await fetch(url, optionsObj);
-    if (!response.ok) throw Error ('Please reload the app')
+    if (!response.ok) throw Error ('Please reload the app');
   } catch (err) {
     errMsg= err.message;
   } finally {
